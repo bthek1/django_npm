1d75ebaa9ba67200c28130b7ffbc7fb307b42b46
Benedict Thekkel <bthekkel1@gmail.com>
2024-11-20 23:04:58 +1000

    added npm, variables not working

--- config/settings.py
+++ config/settings.py
@@ -142,8 +142,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = "/static/"
 
-# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "frontend/css", BASE_DIR / "frontend/js"]
 
 # https://whitenoise.readthedocs.io/en/latest/django.html
 STORAGES = {
